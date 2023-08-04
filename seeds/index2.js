4f19fae4cabe8066f2e6c6e51ddd3dfa11086f64
unknown <shahi.divyansh18@gmail.com>
2023-08-04 21:10:54 +0530

    added unsplash api for images

--- seeds/index2.js
+++ seeds/index2.js
@@ -20,9 +20,13 @@ const seedDB = async()=>{
     await CampGround.deleteMany({});
    for(let i=0; i< 50; i++){
     const random1000=Math.floor(Math.random()*1000);
+    const price=Math.floor(Math.random()*20)+10;
     const camp=new CampGround({
         location: `${cities[random1000].city}, ${cities[random1000].state}`,
-        title: `${sample(descriptors)} ${sample(places)}`
+        title: `${sample(descriptors)} ${sample(places)}`,
+        image: 'https://source.unsplash.com/collection/483251',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nihil neque laboriosam, sit laborum dolorem, accusantium veniam rerum odio ipsa, esse magnam sunt doloribus dignissimos vel delectus laudantium similique aut!',
+        price: price
     })
     await camp.save();
    }
