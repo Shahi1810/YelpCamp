511f944e5938de4a0e5f40f597d5398f259225d7
unknown <shahi.divyansh18@gmail.com>
2023-08-27 00:30:59 +0530

    used app.all() to cater all the requests to wrong urls

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path=require('path');
 const mongoose=require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync =require('./utils/catchAsync')
+const ExpressError=require('./utils/ExpressError')
 const methodOverride=require('method-override');
 const CampGround=require('./models/campground');
 
@@ -38,32 +40,43 @@ app.get('/campgrounds/new', async(req,res)=>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds',async(req,res)=>{
+app.post('/campgrounds',catchAsync(async(req,res,next)=>{
+    if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
     const campground=new CampGround(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.get('/campgrounds/:id', async(req,res)=>{
+app.get('/campgrounds/:id',catchAsync(async(req,res)=>{
     const campground=await CampGround.findById(req.params.id)
     res.render('campgrounds/show',{campground})
-})
+}))
 
-app.get('/campgrounds/:id/edit', async(req,res)=>{
+app.get('/campgrounds/:id/edit', catchAsync(async(req,res)=>{
     const campground=await CampGround.findById(req.params.id)
     res.render('campgrounds/edit',{campground})
-})
+}))
 
-app.put('/campgrounds/:id', async(req,res)=>{
+app.put('/campgrounds/:id', catchAsync(async(req,res)=>{
     const {id}=req.params;
    const campground=await  CampGround.findByIdAndUpdate(id,{...req.body.campground});
    res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete('/campgrounds/:id',async(req,res)=>{
+app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
     const {id}=req.params;
     await CampGround.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.all('*',(req,res,next)=>{
+    next(new ExpressError('page not found', 404))
+})
+
+app.use((err,req,res,next)=>{
+    const {statusCode=500}=err;
+    if(!err.message) err.message='Oh no, Something went wrong!'
+    res.status(statusCode).render('error',{err});
 })
 
 app.listen(3000,()=>{
