87f05cd2ff2cb681b2b1aedbbcb173355f4d0a74
unknown <shahi.divyansh18@gmail.com>
2023-08-28 18:54:02 +0530

    integrated bootstrap to address the errors in a user friendly manner

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path=require('path');
 const mongoose=require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const {campgroundSchema}=require('./schemas.js');
 const catchAsync =require('./utils/catchAsync')
 const ExpressError=require('./utils/ExpressError')
 const methodOverride=require('method-override');
@@ -27,6 +29,16 @@ app.set('views', path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'))
 
+const validateCampground=(req,res,next)=>{
+    const {error}=campgroundSchema.validate(req.body);
+    if(error){
+        const msg =error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
+    }else{
+        next();
+    }
+}
+
 app.get('/', (req,res)=>{
     res.render('home')
 })
@@ -40,8 +52,8 @@ app.get('/campgrounds/new', async(req,res)=>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds',catchAsync(async(req,res,next)=>{
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
+app.post('/campgrounds', validateCampground, catchAsync(async(req,res,next)=>{
+    //if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
     const campground=new CampGround(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -57,7 +69,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async(req,res)=>{
     res.render('campgrounds/edit',{campground})
 }))
 
-app.put('/campgrounds/:id', catchAsync(async(req,res)=>{
+app.put('/campgrounds/:id',validateCampground, catchAsync(async(req,res)=>{
     const {id}=req.params;
    const campground=await  CampGround.findByIdAndUpdate(id,{...req.body.campground});
    res.redirect(`/campgrounds/${campground._id}`)
