bb6bf332c632e8e5d810bbb84f2b669eb586cec3
unknown <shahi.divyansh18@gmail.com>
2023-09-24 19:59:27 +0530

    user authentication and authorization refined and added a routes folder and middleware.js file for cleanup of index.js1

---
+++ routes/reviews.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const router=express.Router({mergeParams: true});
+const {validateReview,isLoggedIn,isReviewAuthor}=require('../middleware')
+const catchAsync=require('../utils/catchAsync');
+const ExpressError=require('../utils/ExpressError');
+const CampGround=require('../models/campground');
+const Review=require('../models/review')
+
+router.post('/',isLoggedIn,validateReview, catchAsync(async(req,res)=>{
+    const campground=await CampGround.findById(req.params.id);
+    const review=new Review(req.body.review);
+    review.author=req.user._id;
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    req.flash('success',' Created a new review');
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+router.delete('/:reviewId',isLoggedIn,isReviewAuthor, catchAsync(async(req,res,next)=>{
+    const{id,reviewId}=req.params;
+    await CampGround.findByIdAndUpdate(id, {$pull:{reviews:reviewId}})
+    await Review.findByIdAndDelete(req.params.reviewId);
+    req.flash('success','Successfully deleted review');
+    res.redirect(`/campgrounds/${id}`)
+}))
+
+module.exports=router;
--- seeds/index2.js
+++ seeds/index2.js
@@ -22,6 +22,7 @@ const seedDB = async()=>{
     const random1000=Math.floor(Math.random()*1000);
     const price=Math.floor(Math.random()*20)+10;
     const camp=new CampGround({
+        author: "64ee2eacabdd00b8c9f8beb7",
         location: `${cities[random1000].city}, ${cities[random1000].state}`,
         title: `${sample(descriptors)} ${sample(places)}`,
         image: 'https://source.unsplash.com/collection/483251',
