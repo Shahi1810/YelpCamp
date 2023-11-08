7bd4ed6e9061022e399ea22437ac1faa1befad93
unknown <shahi.divyansh18@gmail.com>
2023-11-08 16:13:24 +0530

    made a controllers folder to store all the functionalities in it and updated all the routes files accordingly

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,26 +3,10 @@ const router=express.Router({mergeParams: true});
 const {validateReview,isLoggedIn,isReviewAuthor}=require('../middleware')
 const catchAsync=require('../utils/catchAsync');
 const ExpressError=require('../utils/ExpressError');
-const CampGround=require('../models/campground');
-const Review=require('../models/review')
+const reviews=require('../controllers/reviews')
 
-router.post('/',isLoggedIn,validateReview, catchAsync(async(req,res)=>{
-    const campground=await CampGround.findById(req.params.id);
-    const review=new Review(req.body.review);
-    review.author=req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success',' Created a new review');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post('/',isLoggedIn,validateReview, catchAsync(reviews.createReview))
 
-router.delete('/:reviewId',isLoggedIn,isReviewAuthor, catchAsync(async(req,res,next)=>{
-    const{id,reviewId}=req.params;
-    await CampGround.findByIdAndUpdate(id, {$pull:{reviews:reviewId}})
-    await Review.findByIdAndDelete(req.params.reviewId);
-    req.flash('success','Successfully deleted review');
-    res.redirect(`/campgrounds/${id}`)
-}))
+router.delete('/:reviewId',isLoggedIn,isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports=router;
